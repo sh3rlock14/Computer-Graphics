106137c7706e5dcdfeff31d862b91bb7e2793366
sh3rlock14 <mattiacappa@hotmail.it>
2019-05-11 21:24:37 +0200

    Add files via upload

--- functions/fl0/mat2off.m
+++ functions/fl0/mat2off.m
@@ -10,7 +10,7 @@ for file = files'
     [~,name,~] =fileparts(strcat('./',file.name));
     fn = name;
     % se l'oggottp che sto prendendo in considerazione è un file
-    if isfile(strcat('./',fn,'.mat'))
+    if isfile(strcat('./tosca/',fn,'.mat'))
         % creo una struttura per il file .off e la impiego all'interno di
         % "save_as_off".
         % nota: Il file .off ha lo stesso nome del file .mat
